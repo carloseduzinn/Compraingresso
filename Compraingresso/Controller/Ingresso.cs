3f29fef56bba4259973190048a69d6e7e4a1aa0b
carlos.efspires <carlos.efspires@SBC0505243W10-1>
2023-06-19 11:42:17 -0300

    Adicionar arquivos de projeto.

---
+++ Compraingresso/Controller/Ingresso.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compraingresso.Controller
+{
+    internal class Ingresso
+    {
+        public decimal Calcular( decimal valor, decimal qtd)
+        {
+            decimal resul;
+            resul = qtd * valor;
+            return  resul;
+                  
+
+        }
+    }
+    
+    
+
+}
+
+
+
+
---
+++ Compraingresso/View/Telavenda.cs
@@ -0,0 +1,31 @@
+﻿using Compraingresso.Controller;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Compraingresso.View
+{
+    public partial class Telavenda : Form
+    {
+        public Telavenda()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           Ingresso ingresso = new Ingresso();
+            int valor = Convert.ToInt32(textBox2.Text);
+            int qtd = Convert.ToInt32(textBox1.Text);
+             string resul = ingresso.Calcular(valor, qtd).ToString();
+
+            label4.Text = resul; 
+        }
+    }
+}
